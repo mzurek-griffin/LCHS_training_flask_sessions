c9ecd49d5fa0dd0f1a68a25ee6794f26a50a80ca
Jim Flores <jimflores5@gmail.com>
2021-07-25 13:39:45 -0500

    Added New Game option after correct guess.

--- main.py
+++ main.py
@@ -11,25 +11,30 @@ def index():
         guess = int(request.form['guess'])
         low_value = session['low']
         high_value = session['high']
+        session['turns_taken'] += 1
+        still_guessing = True
         if guess < session['magic_number']:
-            message = 'Your guess is too low!'
+            message = f'Your guess ({guess}) is too low.'
             low_value = guess + 1
         elif guess > session['magic_number']:
-            message = 'Your guess is too high!'
+            message = f'Your guess ({guess}) is too high.'
             high_value = guess - 1
         else:
-            message = "VICTORY!"
+            message = f"VICTORY! You took {session['turns_taken']} tries to guess the magic number."
+            still_guessing = False
     else:
         low_value = 1
         high_value = 50
         session['magic_number'] = random.randint(low_value, high_value)
+        still_guessing = True
         message = ''
+        session['turns_taken'] = 0
 
     session['low'] = low_value
     session['high'] = high_value
 
     return render_template('index.html', message = message,
-        low_value = low_value, high_value = high_value)
+        low_value = low_value, high_value = high_value, still_guessing = still_guessing)
 
 if __name__ == '__main__':
     app.run()
