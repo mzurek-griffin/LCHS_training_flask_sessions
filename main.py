53f8e03ead10f78092cad07b03ee80c876e8ee48
Jim Flores <jimflores5@gmail.com>
2021-07-25 13:11:08 -0500

    Removed extraneous starter code.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template
 import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.secret_key = 'SIH*v-6u)c>q<;;h&);cRw,1E_CO8>'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,7 +15,8 @@ def index():
 
         message = ''
 
-    return render_template('index.html', message = message)
+    return render_template('index.html', message = message,
+        low_value = low_value, high_value = high_value)
 
 if __name__ == '__main__':
     app.run()
