95e2dd296d8efed0fd18bc84d37cb2636f2654c4
Matt Z <mzurek@vianney.com>
2021-08-03 09:14:49 -0500

    Added session module and secret key

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.secret_key = 'Lugg@ge_pw:12345'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pass
+        guess = request.form['guess']
+        if guess < magic_number:
+            low_value = guess
+            message = f'{guess} is too low!'
+        elif guess > magic_number:
+            high_value = guess
+            message = f'{guess} is too high!'
+        elif guess == magic_number:
+            message = f'Congratualations!  {guess} was the magic number!'
+            still_guessing = False
+        else:
+            message = 'Please enter a valid input'
+        low_value = 1
+        high_value = 50
+        still_guessing = True
     else:
         low_value = 1
         high_value = 50
